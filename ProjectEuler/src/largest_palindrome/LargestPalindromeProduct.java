6881fbd0e774241d0836f3d73df53c0767d83048
Emanuel Huhnen-Venedey <ehv.dev@posteo.de>
2015-08-04 17:37:02 +0200

    Project Euler problem 4 (largest palindrome product) initialized

---
+++ ProjectEuler/src/largest_palindrome/LargestPalindromeProduct.java
@@ -0,0 +1,18 @@
+package largest_palindrome;
+
+/* Project Euler Problem 4:
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ * https://projecteuler.net/problem=4
+ */
+
+public class LargestPalindromeProduct {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
