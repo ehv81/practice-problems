c8410e453c5597e54651f9d61f20c5109a238ecf
Emanuel Huhnen-Venedey <ehv.dev@posteo.de>
2015-08-07 16:11:17 +0200

    Project Euler Problem 4 (largest palindrome product) solved

--- ProjectEuler/src/largest_palindrome/LargestPalindromeProduct.java
+++ ProjectEuler/src/largest_palindrome/LargestPalindromeProduct.java
@@ -11,8 +11,34 @@ public class LargestPalindromeProduct {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(findLargesPalindrome(3));
+	}
+	
+	public static int findLargesPalindrome(int factorNumberDigits) {
+		if (factorNumberDigits < 1) throw new IllegalArgumentException("number of digits has to be at least 1");
+		int largestPalindromeFound = 1;
+		int k;
+		String stringRepresentation;
+		int l;
+		boolean isPalindrome;
 
+		for (int i = 1; i < Math.pow(10, factorNumberDigits); i++) {
+			for (int j = 1; j < Math.pow(10, factorNumberDigits); j++) {
+				isPalindrome = true;
+				k = i * j;
+				stringRepresentation = Integer.toString(k);
+				l = stringRepresentation.length();
+
+				for (int m = 0; m < l/2; m++) {
+					if (stringRepresentation.charAt(m) != stringRepresentation.charAt(l-1-m)) {
+						isPalindrome = false;
+					}
+				}
+				if (isPalindrome && k > largestPalindromeFound) { largestPalindromeFound = k; }
+			}
+		}
+
+		return largestPalindromeFound;
 	}
 
 }
