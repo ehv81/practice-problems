daec7a2501fdc9b70192adfe02377083fc9d208b
Emanuel Huhnen-Venedey <ehv.dev@posteo.de>
2015-09-18 10:35:39 +0200

    Google Code Jam Problem "File Fix-It" solved

---
+++ GoogleCodeJam/src/file_system_creation/FileSystemCreation.java
@@ -0,0 +1,95 @@
+package file_system_creation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/*
+ * Google Code Jam Problem: File Fix-It
+ * https://code.google.com/codejam/contest/635101/dashboard#s=p0
+ */
+
+public class FileSystemCreation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BufferedReader r;
+		FileWriter fw;
+		String[] buffer;
+		
+		FileSystemNode root;
+		FileSystemNode actNode;
+
+		String inputFilename = "input.txt";
+		String outputFilename = "output.txt";
+		
+		int numberOfExistingDirectories;
+		int numberOfTargetDirectories;
+		int numberOfMkdirInvoked;
+
+		int numberOfCases;
+		int caseNumber = 1;
+		
+		try {
+			r = new BufferedReader(new FileReader(inputFilename));
+			fw = new FileWriter(outputFilename);	
+
+			//The first line of the input contains the number of cases
+			numberOfCases = Integer.parseInt(r.readLine());
+			System.out.printf("Number of cases is %d%n",numberOfCases);
+
+			while( caseNumber <= numberOfCases ){
+				System.out.println("\n" + "-------Case #" + caseNumber + "-------");
+				buffer = r.readLine().split(" ");
+				numberOfExistingDirectories = Integer.parseInt(buffer[0]);
+				numberOfTargetDirectories = Integer.parseInt(buffer[1]);
+				root = new FileSystemNode("/");
+				
+				for (int k = 0; k < numberOfExistingDirectories; k++) {
+					//leading dash results in empty string for buffer[0]
+					buffer = r.readLine().split("/");
+					actNode = root;
+					for (int l = 1; l < buffer.length; l++) {
+						actNode = actNode.decend(buffer[l]);
+					}
+				}
+				
+				numberOfMkdirInvoked = 0;
+				String tmp;
+				
+				for (int k = 0; k < numberOfTargetDirectories; k++) {
+					tmp = r.readLine();
+					System.out.println("\n" + tmp);
+					//leading dash results in empty string for buffer[0]
+					buffer = tmp.split("/");
+					actNode = root;
+					for (int l = 1; l < buffer.length; l++) {
+						if (actNode.mkdirIsNecessary(buffer[l])) {
+							numberOfMkdirInvoked++;
+							System.out.println(buffer[l]);
+						}
+						actNode = actNode.decend(buffer[l]);
+					}
+				}
+
+				fw.write("Case #" + caseNumber + ": " + numberOfMkdirInvoked + "\n");
+
+				caseNumber++;
+			}
+			r.close();
+			fw.close();
+			System.out.printf("File %s successfully processed and results written to %s%n",
+					inputFilename, outputFilename);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ GoogleCodeJam/src/file_system_creation/FileSystemNode.java
@@ -0,0 +1,64 @@
+package file_system_creation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class FileSystemNode {
+	String name;
+	Map<String,FileSystemNode> children;
+
+	FileSystemNode(String name) {
+		this.name = name;
+		children = new HashMap<>();
+	}
+
+	public FileSystemNode decend(String childName) {
+		if (children.keySet().contains(childName))
+			return children.get(childName);
+		else {
+			FileSystemNode newChild = new FileSystemNode(childName);
+			children.put(childName, newChild);
+			return newChild;
+		}
+	}
+	
+	public boolean mkdirIsNecessary(String childName) {
+		if (children.keySet().contains(childName))
+			return false;
+		else return true;  
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FileSystemNode other = (FileSystemNode) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "FileSystemNode [name=" + name + ", number of children=" + children.size() + "]";
+	}
+
+
+}
